75ef2d1c710ca2ce7352c31661a7d32cd9632f9c
raficraft <raficraft@gmail.com>
2022-02-04 01:13:51 +0100

    merge branch features_bdd

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import LanguageProvider from "../context/language/LanguageContext";
 import ModalProvider from "../context/modal/ModalProvider";
 import Layout from "../engine/component/Layout/Layout";
 import DashboardProvider from "../context/Admin/DashboardContext";
-import Db_Provider from "./api/db";
 
 function MyApp({ Component, pageProps }) {
   console.log("pageprops ", pageProps);
@@ -14,11 +13,9 @@ function MyApp({ Component, pageProps }) {
       <LanguageProvider>
         <ModalProvider>
           <DashboardProvider>
-            <Db_Provider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </Db_Provider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
           </DashboardProvider>
         </ModalProvider>
       </LanguageProvider>
